39f54dc1f33c205d6589c5c80ceabf786586c9ec
Bulat Khasanov <90140926+KhasanovBulat@users.noreply.github.com>
2023-11-10 16:20:43 +0300

    Переделано расположение элементов Управления на форме,  Созданы класс библиотеки для работы с MNIST

---
+++ NeuralNetworkForm.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace MNIST_neuralnetwork
+{
+    partial class NeuralNetworkForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.NextButton = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.DetectButton = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.DownlButton = new ComponentFactory.Krypton.Toolkit.KryptonButton();
+            this.DigitTypeLabel = new System.Windows.Forms.Label();
+            this.MNIST_PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MNIST_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 395;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.NextButton);
+            this.splitContainer2.Panel1.Controls.Add(this.DetectButton);
+            this.splitContainer2.Panel1.Controls.Add(this.DownlButton);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.DigitTypeLabel);
+            this.splitContainer2.Panel2.Controls.Add(this.MNIST_PictureBox);
+            this.splitContainer2.Size = new System.Drawing.Size(395, 450);
+            this.splitContainer2.SplitterDistance = 60;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(264, 12);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(120, 32);
+            this.NextButton.TabIndex = 2;
+            this.NextButton.Values.Text = "Следующий";
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // DetectButton
+            // 
+            this.DetectButton.Location = new System.Drawing.Point(138, 12);
+            this.DetectButton.Name = "DetectButton";
+            this.DetectButton.Size = new System.Drawing.Size(120, 32);
+            this.DetectButton.TabIndex = 1;
+            this.DetectButton.Values.Text = "Распознать";
+            // 
+            // DownlButton
+            // 
+            this.DownlButton.Location = new System.Drawing.Point(12, 12);
+            this.DownlButton.Name = "DownlButton";
+            this.DownlButton.Size = new System.Drawing.Size(120, 32);
+            this.DownlButton.TabIndex = 0;
+            this.DownlButton.Values.Text = "Загрузить";
+            this.DownlButton.Click += new System.EventHandler(this.DownlButton_Click);
+            // 
+            // DigitTypeLabel
+            // 
+            this.DigitTypeLabel.AutoSize = true;
+            this.DigitTypeLabel.Location = new System.Drawing.Point(44, 320);
+            this.DigitTypeLabel.Name = "DigitTypeLabel";
+            this.DigitTypeLabel.Size = new System.Drawing.Size(51, 16);
+            this.DigitTypeLabel.TabIndex = 1;
+            this.DigitTypeLabel.Text = "Цифра";
+            // 
+            // MNIST_PictureBox
+            // 
+            this.MNIST_PictureBox.Location = new System.Drawing.Point(47, 22);
+            this.MNIST_PictureBox.Name = "MNIST_PictureBox";
+            this.MNIST_PictureBox.Size = new System.Drawing.Size(301, 262);
+            this.MNIST_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MNIST_PictureBox.TabIndex = 0;
+            this.MNIST_PictureBox.TabStop = false;
+            // 
+            // NeuralNetworkForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "NeuralNetworkForm";
+            this.Text = "NeuralNetworkForm";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MNIST_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton NextButton;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton DetectButton;
+        private ComponentFactory.Krypton.Toolkit.KryptonButton DownlButton;
+        private System.Windows.Forms.Label DigitTypeLabel;
+        private System.Windows.Forms.PictureBox MNIST_PictureBox;
+    }
+}
+
---
+++ NeuralNetworkForm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MNIST_neuralnetwork
+{
+    public partial class NeuralNetworkForm : Form
+    {
+        public NeuralNetworkForm()
+        {
+            InitializeComponent();
+        }
+
+
+        List<Bitmap> images = new List<Bitmap>();
+       // public int currentIndex = 1;
+
+        private void DownloadButton_Click(object sender, EventArgs e)
+        {
+           //// //Random rand = new Random();
+           //// string path = $"{Environment.CurrentDirectory}\\mnist_jpeg\\training\\";
+           //// string[] files_paths = Directory.GetFiles(path);
+
+           //// //foreach (string fileName in files_paths)
+           //// //{
+
+           //// //    Bitmap image = new Bitmap(fileName);
+           //// //    images.Add(image);
+           //// //}
+           //// for(int i=0;i<3;i++)
+           //// {
+           ////     Bitmap image = new Bitmap(files_paths[i]);
+           ////     images.Add(image);
+           //// }
+           //// //string filename = rand.Next(0, 59999).ToString();
+           //// //string full_path = path + filename + "" +".jpeg";
+           //// pictureBox1.Image = images[0];
+           ////// string path_name[]= images[0].ToString().Split();
+           //// PictureNumber.Text = images[0].ToString();
+           //// DownloadButton.Enabled = false;
+
+        }
+
+        
+
+        private void DownlButton_Click(object sender, EventArgs e)
+        {
+            //Random rand = new Random();
+            string path = $"{Environment.CurrentDirectory}\\mnist_jpeg\\training\\";
+            string[] files_paths = Directory.GetFiles(path);
+
+           
+            for (int i = 0; i < 20; i++)
+            {
+                Bitmap image = new Bitmap(files_paths[i]);
+                images.Add(image);
+            }
+            
+            MNIST_PictureBox.Image = images[0];
+            DigitTypeLabel.Text = "0";
+            DownlButton.Enabled = false;
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();   
+            int currentIndex = rand.Next(images.Count);
+                MNIST_PictureBox.Image = images[4];
+                DigitTypeLabel.Text = currentIndex.ToString(); // номер изображения в List<Bitmap>
+        }
+    }
+}
